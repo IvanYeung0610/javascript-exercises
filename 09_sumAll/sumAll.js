e98b8091b2a1e6a8158498d01353cf4e32a28db7
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-08-28 22:59:00 -0400

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    let sum = 0;
+    if (numA < 0 || numB < 0) return 'ERROR';
+    if (numA !== Math.floor(numA) ||
+        numB !== Math.floor(numB)) return 'ERROR';
+    if (numA > numB) {
+        [numA, numB] = [numB, numA];
+    }
+    for (let i = numA; i <= numB; ++i) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
